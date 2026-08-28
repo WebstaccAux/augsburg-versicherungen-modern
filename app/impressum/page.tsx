3af1ff5c3a772a24690afb5e54c7c608b5286fc0
Christian Geier <chris@e0a57058-480f-48c0-993e-8a436ed60934.fritz.box>
2026-08-28 17:47:12 +0200

    Fix imprint page title

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 export const dynamic = 'force-static';
 
 export const metadata: Metadata = {
-  title: 'Impressum | Florian Dietze Versicherungen',
+  title: 'Impressum',
 };
 
 export default function Impressum() {
